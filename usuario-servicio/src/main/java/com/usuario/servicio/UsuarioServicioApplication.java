e9bada14d38fc72af98147610148ca86f4d4d802
Giselle <giselle.carletti@gmail.com>
2023-03-05 09:56:59 -0300

    eureka

--- usuario-servicio/src/main/java/com/usuario/servicio/UsuarioServicioApplication.java
+++ usuario-servicio/src/main/java/com/usuario/servicio/UsuarioServicioApplication.java
@@ -3,9 +3,11 @@ package com.usuario.servicio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UsuarioServicioApplication {
 
 	public static void main(String[] args) {
